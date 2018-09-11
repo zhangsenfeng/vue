a79de25859a83487b66c16d9f6a3b8a33687cdd8
zhangsenfeng <729346129@qq.com>
2018-09-12 01:37:50 +0800

    新增日历控件： 1.默认可查看当前月月历； 2.可设置到指定年份及月份查看该月月历；

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="calendar">
     <div>
-      <button v-on:click="getDate()">更新时间:</button>
-      <span>{{nowDate}}</span>
+      <input type="text" :placeholder="year" v-model="inputYear">
+      <span>年</span>
+      <input type="text" :placeholder="month + 1" v-model="inputMonth">
+      <span>月</span>
     </div>
-    <div>
-      <span>{{year}}年{{month}}月{{date}}日 </span>
-      <span>星期{{day}}</span>
-      <span>{{hour}}时{{minutes}}分{{seconds}}秒</span>
-    </div>
-    <div>
-      <span>.toUTCString(): {{UTCTime}}</span>
-    </div>
-    <div>
-      <span>+new Date(): {{pushDate}}</span>
-    </div>
+    <table>
+      <tr>
+        <th v-for="(week, weekIndex) of formatDay" :key="`week${weekIndex}_cn`" class="title">{{week}}</th>
+      </tr>
+      <tr v-for="(week, weekIndex) of monthCalendar" :key="`week${weekIndex}`">
+        <td v-for="(day, dayIndex) of week" :key="`day${dayIndex}`" :class="{'b_red': day.today, 'thisMonth': day.thisMonth, date}">{{day.date}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -23,23 +22,25 @@ export default {
   data () {
     return {
       nowDate: new Date(),
-      formatDay: {
-        0: '日',
-        1: '一',
-        2: '二',
-        3: '三',
-        4: '四',
-        5: '五',
-        6: '六'
-      }
+      formatDay: ['日', '一', '二', '三', '四', '五', '六'],
+      inputYear: new Date().getFullYear(),
+      inputMonth: new Date().getMonth() + 1
     }
   },
   computed: {
     year () {
-      return this.nowDate.getFullYear()
+      if (!this.inputYear) {
+        return this.nowDate.getFullYear()
+      } else {
+        return this.inputYear
+      }
     },
     month () {
-      return this.nowDate.getMonth() + 1
+      if (!this.inputMonth) {
+        return this.nowDate.getMonth()
+      } else {
+        return this.inputMonth - 1
+      }
     },
     date () {
       return this.nowDate.getDate()
@@ -47,32 +48,122 @@ export default {
     day () {
       return this.formatDay[this.nowDate.getDay()]
     },
-    hour () {
-      return this.nowDate.getHours()
+    // hour () {
+    //   return this.nowDate.getHours()
+    // },
+    // minutes () {
+    //   return this.nowDate.getMinutes()
+    // },
+    // seconds () {
+    //   return this.nowDate.getSeconds()
+    // },
+    // 判断本月第一天是星期几
+    firstDay () {
+      return new Date(this.year, this.month, 1).getDay()
     },
-    minutes () {
-      return this.nowDate.getMinutes()
+    // 判断是否闰年
+    isLaepYear () {
+      if (((this.year % 4 === 0) && (this.year % 100 !== 0)) || (this.year % 400 === 0)) {
+        return true
+      } else {
+        return false
+      }
     },
-    seconds () {
-      return this.nowDate.getSeconds()
+    // 一年中每个月的天数
+    monthDayArr () {
+      const arr = [31, this.isLaepYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      return arr
     },
-    UTCTime () {
-      return this.nowDate.toUTCString()
-    },
-    pushDate () {
-      return +this.nowDate
+    // 月历数组
+    monthCalendar () {
+      // 先创建一个二维数组，外层数组每一项都是数组，代表从周日到周六的七天
+      // 内层数组每一项都是数字，代表是几号
+      let monthArr = []
+      for (let row = 1; row <= 6; row++) {
+        monthArr.push(new Array(7))
+      }
+      // 将本月第一天放入数组
+      monthArr[0][this.firstDay] = {date: 1, thisMonth: true}
+      if (this.date === 1) {
+        monthArr[0][this.firstDay].today = true
+      }
+      // 用上个月的日期填满第一周
+      for (let index = 0; index < 7; index++) {
+        if (!monthArr[0][index]) {
+          const day = this.monthDayArr[this.month >= 1 ? this.month - 1 : 11] - (this.firstDay - index - 1)
+          monthArr[0][index] = {date: day, lastMonth: true}
+        } else {
+          break
+        }
+      }
+      // 将剩余日期填入数组
+      let day = 1
+      let nextMonthDay = 1
+      for (let weekIndex = 0; weekIndex < 6/* 一共六周 */; weekIndex++) {
+        for (let dayIndex = 0; dayIndex < 7/* 每周七天 */; dayIndex++) {
+          if (!monthArr[weekIndex][dayIndex]) {
+            if (day < this.monthDayArr[this.month]) {
+              // 将本月的日期填入数组
+              day++
+              monthArr[weekIndex][dayIndex] = {date: day, thisMonth: true}
+              if (this.date === day) {
+                monthArr[weekIndex][dayIndex].today = true
+              }
+            } else {
+              // 用下月日期填满剩余数组
+              monthArr[weekIndex][dayIndex] = {date: nextMonthDay, lastMonth: true}
+              nextMonthDay++
+            }
+          }
+        }
+      }
+      return monthArr
     }
   },
-  created () {
-    // setInterval(() => {
-    //   this.nowDate = new Date()
-    // }, 1000)
+  watch: {
+    inputYear (value) {
+      const number = this.digitalCheck(value, 0)
+      this.inputYear = number
+    },
+    inputMonth (value) {
+      const number = this.digitalCheck(value, 0, 12)
+      this.inputMonth = number
+    }
   },
   methods: {
-    getDate () {
-      const nowDate = new Date()
-      this.nowDate = nowDate
+    // 数字校验
+    digitalCheck (value, min, max) {
+      // 将value转换为字符串
+      value += ''
+      // 去除数字外的其他字符
+      value = value.replace(/[^0-9]/g, '')
+      // 去除字符串前的0
+      value = value.replace(/^0+/, '')
+      // 将字符串转为数字
+      parseInt(value, 10)
+      if (min && value <= min) {
+        value = min
+      }
+      if (max && value >= max) {
+        value = max
+      }
+      return value
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.b_red {
+  background-color: red;
+}
+th, td {
+  text-align: center;
+}
+td.date:not(.thisMonth) {
+  color: #999;
+}
+input {
+  width: 50px;
+}
+</style>
